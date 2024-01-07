421aaf9a3d1d5fb4f43b35079e7574bd015d65b3
ibr4himeren <116194310+ibr4himeren@users.noreply.github.com>
2024-01-07 14:07:55 +0300

    Create assets.java

---
+++ assets.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ibrahim_Gökdemir;
+
+/**
+ *
+ * @author ieren
+ */
+public class assets {
+
+    String name;
+    String serialno;
+    String acquisitiondate;
+    String value;
+    String category;
+    String fragile;
+   
+}
