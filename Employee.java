514f1339512d23f267cfdcc1310d8f9f7eab4e8d
ibr4himeren <116194310+ibr4himeren@users.noreply.github.com>
2024-01-07 14:05:42 +0300

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ibrahim_Gökdemir_2221251372_cp2_p1;
+
+/**
+ *
+ * @author ieren
+ */
+public class Employee {    
+    String name;
+    String surname;
+    String email;
+    String password;
+    String address;
+    String phonenumber;
+    String Employeetype;
+    String gender;
+}
